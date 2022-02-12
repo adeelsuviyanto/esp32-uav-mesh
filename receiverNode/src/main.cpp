6b92ba24d6f6da1f9c920472c18a83bbadaf3a8d
adeelsuviyanto <adeelsuviyanto@gmail.com>
2022-02-12 20:55:01 +0700

    v1.0 First Revision from December 2021

---
+++ receiverNode/src/main.cpp
@@ -0,0 +1,203 @@
+/*
+  * ESP32-based Inter-UAV Communications System
+  * Muhammad Adeel Mahdi Suviyanto
+  * Telkom University
+  * 
+  * 2nd receiver implementation, new algorithm
+  * 
+  * Changelog:
+  * v0.1, 2021-11-19: First implementation of receiver node and painlessMesh
+  * v0.2, 2021-11-24: Slight revision to algorithm
+  * v1.0, 2021-11-29: New algorithm, polling from receiver instead of sender
+  * 
+  * Version 1.0, 2021-11-29
+  * Node 2 (receiver)
+*/
+#include <Arduino.h>
+//#include <ArduinoJson.h>
+#include "painlessMesh.h"
+#include "esp_wifi.h"
+#include <WiFi.h>
+#include <TaskScheduler.h>
+#include <TaskSchedulerDeclarations.h>
+
+//Mesh settings
+#define MESH_PREFIX "UAV"
+#define MESH_PASS "12345678"
+#define MESH_PORT 5555
+
+//Node number for current board
+int nodeNumber = 2;
+
+//Declarations
+Scheduler userScheduler;
+painlessMesh mesh;
+int i = 0;
+
+int error;
+double currentLatitude;
+double currentLongitude;
+double currentAltitude;
+int currentSatellites;
+int currentGPSDate;
+int currentGPSTime;
+long rssi;
+
+unsigned long timing;
+
+struct{
+  uint32_t senderNodeId;
+  boolean gpsReady = false;
+}senderNodes;
+uint32_t destNode;
+
+void requestLocation();
+void queryGPSStatus();
+void queryNodeType();
+
+Task taskReqQuery(TASK_SECOND * 10, -1, &queryGPSStatus );
+Task taskQueryNode(TASK_SECOND * 5, -1, &queryNodeType );
+Task taskReqLocation(TASK_SECOND * 5, -1, &requestLocation );
+
+void requestLocation(){
+  if(taskReqQuery.isEnabled() == true) taskReqQuery.disable();
+  Serial.printf("\nRequesting location data from sender..");
+  String msg = "reqLocation";
+  mesh.sendSingle(senderNodes.senderNodeId, msg);
+  Serial.printf("\nWiFi RSSI value: %li dBm", rssi);
+}
+
+void queryGPSStatus(){
+  if(taskQueryNode.isEnabled() == true) taskQueryNode.disable();
+  Serial.printf("\nQuerying GPS status from sender..");
+  String msg = "gpsStatus";
+  mesh.sendSingle(senderNodes.senderNodeId, msg);
+  Serial.printf("\nWiFi RSSI value: %li dBm", rssi);
+}
+
+void queryNodeType(){
+  Serial.printf("\nQuerying node type...");
+  Serial.println(millis());
+  String msg = "getType";
+  mesh.sendSingle(destNode, msg);
+  Serial.printf("\nWiFi RSSI value: %li dBm", rssi);
+}
+
+void acknowledge(uint32_t toNode){
+  Serial.println("Sending acknowledge..");
+  String msg = "OK Location Data received from ";
+  msg = msg + toNode;
+  mesh.sendSingle(toNode, msg);
+}
+
+void newConnectionCallback(uint32_t nodeId){
+  Serial.printf("New Connection, nodeId = %u\n", nodeId);
+  destNode = nodeId;
+  if(taskQueryNode.isEnabled() == false) {
+    taskQueryNode.enable();
+    Serial.printf("taskQueryNode %s", taskQueryNode.isEnabled() ? "enabled" : "disabled");
+    ;
+  }
+  else {
+    queryNodeType();
+    Serial.printf("taskQueryNode is not enabled");
+  }
+}
+
+void receivedCallback(uint32_t from, String &msg){
+  Serial.printf("\nMessage from %u msg=%s\n", from, msg.c_str());
+  long int senderRSSI;
+  //JSONVar recObj = JSON.parse(msg.c_str()); //parse JSON string to objects
+  DynamicJsonDocument JsonDocument(1024 + msg.length());
+  JsonDocument.clear();
+  deserializeJson(JsonDocument, msg);
+  JsonObject recObj = JsonDocument.as<JsonObject>();
+  senderRSSI = recObj["rssi"];
+  Serial.printf("\nClient-side RSSI: %li dBm\n", senderRSSI);
+  if(recObj.containsKey("nodeType")){ //check if sender node sent "sender" after query node type
+    String nodeType = recObj["nodeType"];
+    if(nodeType.equals("sender") == true){
+      senderNodes.senderNodeId = from;
+      Serial.printf("senderNodes.senderNodeId = %u", from);
+      taskReqQuery.enable();
+      ;
+      //queryGPSStatus();
+    }
+    Serial.printf("\nAdded node %u to sender nodes", from);
+  }
+  if(recObj.containsKey("locationReady")){
+    uint8_t locationReady = recObj["locationReady"];
+    if(locationReady == 1){
+      senderNodes.gpsReady = true;
+      taskReqQuery.disable();
+      taskReqLocation.enable();
+      ;
+    }
+    else{
+      senderNodes.gpsReady = false;
+      if(taskReqLocation.isEnabled() == true )taskReqLocation.disable();
+    }
+    Serial.printf("Sender GPS ready: %s", senderNodes.gpsReady ? "true" : "false");
+  }
+  else{
+    if(senderNodes.gpsReady == true){
+      int node;
+      double latitude;
+      double longitude;
+      double altitude;
+      int satellites;
+      node = recObj["node"];
+      Serial.println(node);
+      latitude = recObj["latitude"];
+      Serial.println(latitude, 6);
+      longitude = recObj["longitude"];
+      Serial.println(longitude, 6);
+      altitude = recObj["altitude"];
+      Serial.println(altitude);
+      satellites = recObj["satellites"];
+      Serial.println(satellites);
+      if(taskQueryNode.isEnabled() == true) taskQueryNode.disable();
+      acknowledge(from);
+    }
+  }
+  JsonDocument.clear();
+}
+
+void changedConnectionCallback() {
+  Serial.printf("Changed connections\n");
+  if(taskQueryNode.isEnabled() == false) taskQueryNode.enable();
+  else queryNodeType();
+}
+
+void nodeTimeAdjustedCallback(int32_t offset) {
+  Serial.printf("Adjusted time %u. Offset = %d\n", mesh.getNodeTime(),offset);
+}
+
+void setup() {
+  // put your setup code here, to run once:
+  Serial.begin(115200);
+  mesh.setDebugMsgTypes (ERROR | STARTUP | CONNECTION);
+  mesh.init(MESH_PREFIX, MESH_PASS, MESH_PORT);
+  esp_wifi_set_protocol(WIFI_IF_AP, WIFI_PROTOCOL_LR);
+  esp_wifi_set_protocol(WIFI_IF_STA, WIFI_PROTOCOL_LR);
+  mesh.onReceive(&receivedCallback);
+  mesh.onNewConnection(&newConnectionCallback);
+  mesh.onChangedConnections(&changedConnectionCallback);
+  mesh.onNodeTimeAdjusted(&nodeTimeAdjustedCallback);
+  
+  userScheduler.addTask(taskQueryNode);
+  userScheduler.addTask(taskReqQuery);
+  userScheduler.addTask(taskReqLocation);
+  userScheduler.enable();
+  //taskReqQuery.enable();
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  mesh.update();
+  userScheduler.execute();
+  rssi = WiFi.RSSI();
+  //Serial.printf("\ntaskQueryNode %s", taskQueryNode.isEnabled() ? "true" : "false");
+  //Serial.printf("\ntaskReqQuery %s", taskReqQuery.isEnabled() ? "true" : "false");
+}
---
+++ senderNode/src/senderNode.cpp
@@ -0,0 +1,158 @@
+/*
+ * ESP32-based Inter-UAV Communications System
+ * Muhammad Adeel Mahdi Suviyanto
+ * Telkom University
+ * 
+ * 3rd GPS implementation, replace algorithm
+ * 
+ * Version 1.0, 2021-12-04
+ * 
+ * Node 1
+ */
+
+#include <Arduino.h>
+#include "painlessMesh.h"
+#include "esp_wifi.h"
+//#include <Arduino_JSON.h>
+#include <WiFi.h>
+#include <TinyGPS++.h>
+#include <SoftwareSerial.h>
+
+//Mesh details
+#define MESH_PREFIX "UAV" //name of mesh
+#define MESH_PASS "12345678" //mesh password
+#define MESH_PORT 5555
+
+//Node number for current board
+int nodeNumber = 1;
+
+//Location data to send
+String locationData;
+
+//Declarations
+static const int RXPin = 16, TXPin = 17;
+static const uint32_t GPSBaud = 9600;
+Scheduler userScheduler;
+painlessMesh mesh;
+TinyGPSPlus gps;
+SoftwareSerial ss(RXPin, TXPin);
+void sendMessage();
+String getLocation();
+
+//Variables
+double currentLatitude;
+double currentLongitude;
+double currentAltitude;
+int currentSatellites;
+int currentGPSDate;
+int currentGPSTime;
+uint32_t receiverNode;
+long rssi;
+
+void showGPSData();
+Task taskShowGPSData(TASK_SECOND, -1, &showGPSData);
+
+void getLocation(uint32_t toNode){
+    locationData = "";
+    Serial.println("Sending Location Data");
+    DynamicJsonDocument locDat(1024);
+    locDat["latitude"] = currentLatitude;
+    locDat["longitude"] = currentLongitude;
+    locDat["altitude"] = currentAltitude;
+    locDat["satellites"] = currentSatellites;
+    locDat["rssi"] = rssi;
+    serializeJson(locDat, locationData);
+    mesh.sendSingle(receiverNode, locationData);
+    Serial.print("Sent message: ");
+    Serial.println(locationData);
+    locDat.clear();
+}
+
+void showGPSData(){
+    Serial.printf("Latitude: %f\n", currentLatitude);
+    Serial.printf("Longitude: %f\n", currentLongitude);
+    Serial.printf("Altitude: %f\n", currentAltitude);
+    Serial.printf("Satellites: %i\n", currentSatellites);
+}
+
+void replyNodeQuery(uint32_t toNode){
+    Serial.println("Sending Node Type");
+    receiverNode = toNode;
+    String msg;
+    DynamicJsonDocument JsonDocument(1024);
+    JsonObject reply = JsonDocument.to<JsonObject>();
+    reply["nodeType"] = "sender";
+    reply["rssi"] = rssi;
+    serializeJson(reply, msg);
+    mesh.sendSingle(toNode, msg);
+    JsonDocument.clear();
+}
+
+void replyGPSQuery(uint32_t toNode){
+    Serial.println("Sending GPS Status Query");
+    String msg;
+    DynamicJsonDocument reply(1024);
+    if(currentSatellites < 3 || (currentLatitude == 0 && currentLongitude == 0 && currentAltitude == 0)) reply["locationReady"] = 0;
+    else reply["locationReady"] = 1;
+    reply["rssi"] = rssi;
+    serializeJson(reply, msg);
+    mesh.sendSingle(toNode, msg);
+    Serial.print("Sent message: ");
+    Serial.println(msg);
+    reply.clear();
+}
+
+void newConnectionCallback(uint32_t nodeId){
+    Serial.printf("New Connection, nodeId = %u\n", nodeId);
+}
+
+void changedConnectionCallback() {
+    Serial.printf("Changed connections\n");
+}
+
+void nodeTimeAdjustedCallback(int32_t offset) {
+    Serial.printf("Adjusted time %u. Offset = %d\n", mesh.getNodeTime(),offset);
+}
+
+void receivedCallback(uint32_t from, String &msg){
+    Serial.printf("Message from %u msg=%s\n", from, msg.c_str());
+    if (strcmp(msg.c_str(), "getType") == 0) replyNodeQuery(from);
+    if (strcmp(msg.c_str(), "gpsStatus") == 0) replyGPSQuery(from);
+    if (strcmp(msg.c_str(), "reqLocation") == 0) getLocation(from);
+}
+
+void setup(){
+    Serial.begin(115200);
+    ss.begin(GPSBaud);
+    mesh.setDebugMsgTypes( ERROR | STARTUP | CONNECTION);
+    mesh.init(MESH_PREFIX, MESH_PASS, MESH_PORT);
+    esp_wifi_set_protocol(WIFI_IF_AP, WIFI_PROTOCOL_LR);
+    esp_wifi_set_protocol(WIFI_IF_STA, WIFI_PROTOCOL_LR);
+    mesh.onReceive(&receivedCallback);
+    mesh.onNewConnection(&newConnectionCallback);
+    mesh.onChangedConnections(&changedConnectionCallback);
+    mesh.onNodeTimeAdjusted(&nodeTimeAdjustedCallback);  
+    userScheduler.addTask(taskShowGPSData);
+    taskShowGPSData.enable();  
+}
+
+void loop(){
+    mesh.update();
+    userScheduler.execute();
+    while(ss.available() > 0){
+        gps.encode(ss.read());
+    }
+    if(millis() > 5000 && gps.charsProcessed() < 10){
+        Serial.println("Error: No GPS data received. Check wiring and reset.");
+        Serial.println("Board will now dump GPS stream: ");
+        while(true)
+        if(ss.available() > 0) {
+            Serial.write(ss.read());
+        }
+    }
+    currentLatitude = (gps.location.lat());
+    currentLongitude = (gps.location.lng());
+    currentAltitude = (gps.altitude.meters());
+    currentSatellites = (gps.satellites.value());
+    rssi = WiFi.RSSI();
+}
